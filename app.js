8dddd4af7dced11e8cd30b1dbf07b0ba0812304f
Arthur Battut <arthur@soixantecircuits.fr>
2016-03-07 11:58:37 +0100

    update final size of image to be divided by two

--- app.js
+++ app.js
@@ -6,12 +6,18 @@ console.log('Caleb is gonna transform your images')
 function calibrateImg (img, size, calib, index) {
 	var imgCalib = calib.computed_transforms[index]
 	var imgName = (String(index).length < 2) ? '0'+ index : index
+
+	var finalCropSize = {height: 0, width: 0}
+	finalCropSize.height = (calib.global_parameters.min_crop_height % 2 === 0 ) ? calib.global_parameters.min_crop_height : calib.global_parameters.min_crop_height - 1
+	finalCropSize.width = (calib.global_parameters.min_crop_width % 2 === 0 ) ? calib.global_parameters.min_crop_width : calib.global_parameters.min_crop_width - 1
+
+	// Transform image
 	img
 	.rotate('#000000', -imgCalib.angle)
 	.gravity('Center')
 	.scale((imgCalib.scale*100)+'%', (imgCalib.scale*100)+'%')
 	.crop(imgCalib.cropWidth, imgCalib.cropHeight, -imgCalib.offsetX, -imgCalib.offsetY)
-	.crop(calib.global_parameters.min_crop_width, calib.global_parameters.min_crop_height, 0, 0)
+	.crop(finalCropSize.width, finalCropSize.height, 0, 0)
 	.write('./aligned/aligned_'+imgName+'.jpg', function (err) {
 	  if (err) {
 	    console.log(err)
@@ -25,6 +31,12 @@ function calibrateImg (img, size, calib, index) {
 // Get calibration data
 var calibData = JSON.parse(fs.readFileSync('calibration_data.json', 'utf8'))
 var imgPaths = fs.readdirSync('./source_img')
+var alignedImgs = fs.readdirSync('./aligned')
+
+// Remove presents aligned files to avoid bugs
+for (var i = 0; i < alignedImgs.length; i++) {
+	fs.unlinkSync('./aligned/'+alignedImgs[i])
+}
 
 for (var i = 0; i < imgPaths.length; i++) {
 	var img = gm('./source_img/'+imgPaths[i])
