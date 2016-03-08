c0618a12f6e17c5f9dfef0b9a4a995273120a202
Arthur Battut <arthur@soixantecircuits.fr>
2016-03-08 17:35:33 +0100

    add transformation with imagemagick

--- app.js
+++ app.js
@@ -14,40 +14,55 @@ function sign (x) {
 
 function calibrateImg(img, size, calib, index) {
   var imgCalib = calib.computed_transforms[index]
-  var signedX = (sign(imgCalib.offsetX) > 0) ? '+' : ''
-  var signedY = (sign(imgCalib.offsetY) > 0) ? '+' : ''
-  console.log(imgCalib.cropWidth + 'x' + imgCalib.cropHeight + signedX + imgCalib.offsetX + signedY + imgCalib.offsetY)
-  
+  var imgName = (String(index).length < 2) ? '0'+ index : index
+
   im.convert([
     img.source,
     '-distort',
-    'SRT', imgCalib.scale + ',' + imgCalib.angle,
-    '-crop',
-    imgCalib.cropWidth + 'x' + imgCalib.cropHeight + signedX + imgCalib.offsetX + signedY + imgCalib.offsetY,
-    '+repage',
-    path.join(pathDestinationDir,'aligned_' + index + '.jpg')
+    'SRT', imgCalib.scale + ',' + -imgCalib.angle,
+    '+repage', 
+    path.join(pathDestinationDir,'aligned_' + imgName + '.jpg')
   ],
-    function (err, stdout) {
-      if (err) throw err
-      console.log('stdout:', stdout)
-      console.log(calib.global_parameters.min_crop_width)
-      console.log(calib.global_parameters.min_crop_height)
-      gm(path.join(pathDestinationDir,'aligned_' + index + '.jpg'))
-      .crop(calib.global_parameters.min_crop_width, calib.global_parameters.min_crop_height, 0, 0)
-      .write(img.source, function (err) {
-        if (err) {
-          console.log(err)
-        } else {
-          console.log('Done transforming img: ' + img.source)
-        }
-      })
+  function (err, stdout) {
+    if (err) throw err
+    var imgCalib = calib.computed_transforms[index]
+    var imgName = (String(index).length < 2) ? '0'+ index : index
+    var imgPath = path.join(pathDestinationDir,'aligned_' + imgName + '.jpg')
+
+    gm(imgPath)
+    .gravity('Center')
+    .crop(imgCalib.cropWidth, imgCalib.cropHeight, -imgCalib.offsetX, -imgCalib.offsetY)
+    .crop(calib.global_parameters.min_crop_width, calib.global_parameters.min_crop_height, 0, 0)
+    .write(imgPath, function (err) {
+      if (err) {
+        console.log(err)
+      } else {
+        console.log('Done transforming img: ' + img.source)
+      }
     })
+  })
 }
 
+
+// Setup 
+try {
+  fs.mkdirSync('./aligned')
+  console.log('Creating aligned folder')
+} catch(e) {}
+try {
+  fs.mkdirSync('./source_img')
+  console.log('Creating source_img folder')
+} catch(e) {}
 
 // Get calibration data
 var calibData = JSON.parse(fs.readFileSync('calibration_data.json', 'utf8'))
 var imgPaths = fs.readdirSync(pathDir)
+var alignedImgs = fs.readdirSync('./aligned')
+
+// Remove presents aligned files to avoid bugs
+for (var i = 0; i < alignedImgs.length; i++) {
+  fs.unlinkSync('./aligned/'+alignedImgs[i])
+}
 
 for (var i = 0; i < imgPaths.length; i++) {
   var img = gm(path.join(pathDir, imgPaths[i]))
